4941ffcabff68a0604a337938a917d641e328560
ramya shah <75496156+ramyashah27@users.noreply.github.com>
2021-01-21 16:11:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,153 @@
+                            // 1. Ways to print in JavaScript
+console.log("Hello World");
+// alert("me");
+// document.write("this is document write")
+
+                            // 2. Javascript console API
+    // console.log("Hello World", 4 + 6, "Another log");
+    // console.warn("this is warning");
+    // console.error("This is an error");
+    // console.assert(5+5==10)
+
+                            // 3.java script VARIABLES
+// var number1 = 31
+// var number2 = 45
+// console.log( number1 + number2)
+                            // 4. Data types in JavaScript
+// Numbers
+// var num1 = 455;
+// var num2 = 56.76;
+
+// String
+// var str1 = "This is a string";
+// var str2 = 'This is also a string';
+
+// Objects
+// var marks = {
+//     ravi: 34,
+//     shubham: 78,
+//     ramyashah: 99.977
+// }
+// var trailerr = {
+//     ramya : 'OverPowered',
+//     byjus : 'teacher'
+// var a1 = "ramya"
+// var b1 = "shah"
+// console.log(a1 + b1)
+// }
+// console.log(trailerr);
+// booleans
+// console.log(marks);
+// var a = true;
+// var b = false;
+
+// console.log(a,b);
+// und
+// var und;
+// console.log(und)    
+// var n = null
+// console.log(n)
+
+// at very high level programming there are two typee of data types in js
+// 1. primitive datatypes --> undefined , null , nos. strings , bolleans , symobl
+// 2. relative datatypes --> array and object 
+
+// var arr = [ 1, 2, 3 ,4, "ramya",  5,6]
+// console.log(arr)
+// console.clear()
+
+
+
+
+// arthemtic operators :
+// var a = 9000  
+// var b = 1000
+// console.log("the value of a and b is", a+b)
+// console.log("the value of a and b is", a-b)
+// console.log("the value of a and b is", a*b)
+// console.log("the value of a and b is", a/b)
+                                    // ASSIGNMENT OPERATORS
+// var a = 50
+// b = a
+// b += 8
+// console.log(b)
+
+
+
+// // bollean and comparsion operators
+// var a = 10
+// var b = 30
+// console.log(a==b)
+// console.log(a>=b)
+// console.log(a<=b)
+
+// Logical Operators
+
+// Logical and
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+// Logical or
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// Logical not
+// console.log(!false);
+// console.log(!true);
+
+                                    // FUNCTIONs IN Js
+
+// function avg(a1,a2){
+//     return (a1+a2)/2
+// }
+// t1 = avg(4,6)
+// t12 = avg(5,81)
+// console.log(t1, t12)
+
+
+
+// CONDITIONAL 
+
+// var age= 4
+// // if (age>=18){
+// //     console.log("big men")
+
+// // }
+// // else {
+// //     console.log("kids ")
+// // }
+
+// if (age>18){
+//     console.log("18 plus")
+// }
+// else if (age==4){
+//     console.log("4 sale kea programmer")
+// }
+// taking input extra
+// var name = window. prompt("Enter your name: ")
+// if (name>18){
+//     console.log('hello')
+// }
+
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     if(i==2){
+//         break;
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+
+// let j = 0 
+// while(j < arr.length){
+//     console.log(arr[j])
+//     j++
+// }
+
+let myarr = [ 1, 2 , " ramya shah " , "popatlal ", 6]
+console.log(myarr)
